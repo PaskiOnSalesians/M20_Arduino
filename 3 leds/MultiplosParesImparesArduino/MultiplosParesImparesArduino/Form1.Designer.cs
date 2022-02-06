a3a0f84bf5b1e0e953e4d746c34b338c2c467a95
nomissolrac <61462036+Ahegaox@users.noreply.github.com>
2022-02-06 18:23:54 +0100

    multiplos visual

---
+++ 3 leds/MultiplosParesImparesArduino/MultiplosParesImparesArduino/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace MultiplosParesImparesArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblPuerto = new System.Windows.Forms.Label();
+            this.LblNumero = new System.Windows.Forms.Label();
+            this.LblMultiplo = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblPuerto
+            // 
+            this.LblPuerto.AutoSize = true;
+            this.LblPuerto.Location = new System.Drawing.Point(120, 89);
+            this.LblPuerto.Name = "LblPuerto";
+            this.LblPuerto.Size = new System.Drawing.Size(54, 17);
+            this.LblPuerto.TabIndex = 0;
+            this.LblPuerto.Text = "Puerto:";
+            // 
+            // LblNumero
+            // 
+            this.LblNumero.AutoSize = true;
+            this.LblNumero.Location = new System.Drawing.Point(120, 163);
+            this.LblNumero.Name = "LblNumero";
+            this.LblNumero.Size = new System.Drawing.Size(62, 17);
+            this.LblNumero.TabIndex = 1;
+            this.LblNumero.Text = "Numero:";
+            // 
+            // LblMultiplo
+            // 
+            this.LblMultiplo.AutoSize = true;
+            this.LblMultiplo.Location = new System.Drawing.Point(120, 239);
+            this.LblMultiplo.Name = "LblMultiplo";
+            this.LblMultiplo.Size = new System.Drawing.Size(56, 17);
+            this.LblMultiplo.TabIndex = 2;
+            this.LblMultiplo.Text = "Multiplo";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(231, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(231, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(409, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(383, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.LblMultiplo);
+            this.Controls.Add(this.LblNumero);
+            this.Controls.Add(this.LblPuerto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblPuerto;
+        private System.Windows.Forms.Label LblNumero;
+        private System.Windows.Forms.Label LblMultiplo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 3 leds/MultiplosParesImparesArduino/MultiplosParesImparesArduino/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace MultiplosParesImparesArduino
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ArduinoPort;
+        string[] PuertosDisponibles;
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PuertosDisponibles = SerialPort.GetPortNames();
+
+            foreach (var item in PuertosDisponibles)
+            {
+                comboBox1.Items.Add(item);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArduinoPort = new SerialPort();
+            ArduinoPort.PortName = comboBox1.GetItemText(comboBox1.SelectedItem);
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+        }
+
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    Write(Int64.Parse(textBox1.Text)#Int64.Parse(textBox2.Text));
+        //    int resultado;
+        //    for (int i = 0; i < Int64.Parse(textBox1.Text); i++)
+        //    {
+
+        //        resultado = i/Int32.Parse(textBox2.Text);
+        //        if (resultado%2==0)
+        //        {
+                    
+        //        }
+        //    }
+        //}
+    }
+}
